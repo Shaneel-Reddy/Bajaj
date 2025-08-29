d3155d62f3552fce31b543140140227f9a18ac40
Shaneel-Reddy <shaneelreddy320@gmail.com>
2025-08-29 10:36:33 +0530

    Initialize Spring Boot project with Maven wrapper and basic application structure

---
+++ webhook-challenge/src/main/java/com/bajajfinserv/webhook_challenge/WebhookChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.bajajfinserv.webhook_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookChallengeApplication.class, args);
+	}
+
+}
---
+++ webhook-challenge/src/test/java/com/bajajfinserv/webhook_challenge/WebhookChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajajfinserv.webhook_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
